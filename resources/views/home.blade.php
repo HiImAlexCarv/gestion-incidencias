a8fd44c255b54a72bc6f3facccaec8c179cd4acb
alexgtf1990@gmail.com <axelrmcf@gmail.com>
2023-05-12 19:01:58 +0200

    creacion del proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Bienvenido - Sistema de gestion de incidencias
+                    <div class="panel-body">
+                        <p>Hola</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
